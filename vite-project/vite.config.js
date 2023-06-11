2ab491ca602280209d4d7e7d070dbf0361938823
Modx <122527984+shazaaam101@users.noreply.github.com>
2023-06-11 23:52:05 +0700

    Edit file name

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://shazaaam101.github.io/interactive-pricing-component",
+  base: "https://shazaaam101.github.io/interactive-pricing",
   plugins: [react()],
 });
